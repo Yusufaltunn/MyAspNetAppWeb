1d4e1a46e88b815c12f7425a61fca52ad1cb7244
Yusufaltunn <yusuf99altun@hotmail.com>
2023-10-06 20:15:26 +0300

    Stringlenght

--- MyAspNetAppWeb/ViewModels/ProductViewModel.cs
+++ MyAspNetAppWeb/ViewModels/ProductViewModel.cs
@@ -5,6 +5,7 @@ namespace MyAspNetAppWeb.ViewModels
     public class ProductViewModel
     {
           public int Id { get; set; }
+        [StringLength(50,ErrorMessage ="isim alanına en fazla 50 karakter gireblir")]
         [Required(ErrorMessage ="isim alanı boş olamaz")]
         public string? Name { get; set; }
         
@@ -16,6 +17,7 @@ namespace MyAspNetAppWeb.ViewModels
         [Range(1,200,ErrorMessage = "Stok alanı 1 ile 200 arasında değer olmalıdır")]
         public int? Stock { get; set; }
         [Required(ErrorMessage = "açıklama alanı boş olamaz")]
+        [StringLength(300,MinimumLength =50, ErrorMessage = "Açıklama alanına en fazla 300 karakter gireblir")]
 
         public string Description { get; set; }
 
